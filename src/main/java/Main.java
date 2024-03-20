16bfc1d197b8f76d8b5a568167c7e28a36651de3
MikKrochmal <162459334+MikKrochmal@users.noreply.github.com>
2024-03-20 14:07:34 +0000

    poprawa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) 
 {
-   int min=100000;
-   int[] liczba = new int[5];
-   Scanner scanner = new Scanner(System.in);
+  Scanner scanner = new Scanner(System.in);
+   int min=Integer.MAX_VALUE;
+   
    for(int i=0;i<5;i++)
      {
-       liczba[i] = scanner.nextInt();
+  system.out.println("Enter a number" +(i+1)+":");
+       int number=scanner.nextInt();
+       if(number<min)
+       {
+         min=number;
+       }
+       
      }
-   for(int i=0;i<5;i++)
-     {
-       if(liczba[i]<min) min=liczba[i];
-     }
-  System.out.println(min);
+  system.out.println("The minimum number is:"+min);
+}  
 }
